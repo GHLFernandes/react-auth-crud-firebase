26ac4627875e1905f928bc24425e8cadb56b49ba
Gabriel Fernandes <fernandes.gh@outlook.com>
2023-01-25 00:41:28 -0300

    feat: config

---
+++ src/config/config.ts
@@ -0,0 +1,12 @@
+const config = {
+    firebase: {
+        apiKey: "AIzaSyA7ss0DemVgS_h4npWvFPyH1wA12wV9DTw",
+        authDomain: "gf-desenvolvimento.firebaseapp.com",
+        projectId: "gf-desenvolvimento",
+        storageBucket: "gf-desenvolvimento.appspot.com",
+        messagingSenderId: "920626529228",
+        appId: "1:920626529228:web:d73d98d4a4187fce97d369"
+    }
+}
+
+export default config;
